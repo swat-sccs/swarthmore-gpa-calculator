170d607866f175b74eaa97f240e54517d711eb68
Oliver Newman <oliver@whmsi.com>
2016-11-03 19:41:51 -0400

    Initial commit

---
+++ gpa_calc.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+                  render_template, flash
+from helpers import parse_grades, calculate_gpa
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+# app.config.from_envvar('GPA_CALC_SETTINGS', silent=True)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        raw_grades = request.form['grades']
+        try:
+            grades = parse_grades(raw_grades)
+            gpa = calculate_gpa(grades)
+        except KeyError:
+            return render_template('home.html', notice="Invalid grades")
+
+        return render_template('gpa.html', gpa=gpa)
+    else:
+        return render_template('home.html')
+
+
+
---
+++ helpers.py
@@ -0,0 +1,56 @@
+def parse_grades(raw_grades):
+    """Convert from a string of grades to a structured dict.
+
+    Args:
+        raw_grades
+    Returns
+    """
+    # Split raw data up by line, and filter out column label lines
+    lines = raw_grades.split("\n")
+    is_course_line = lambda l: not (l.startswith("Term") or l.startswith("Course"))
+    courses = list(filter(is_course_line, lines))
+
+    # Convert each course row to a dict
+    course_list = []
+    for course in courses:
+        course_info = course.split("\t") # Split up fields of row
+        course_fields = ['course', 
+                         'title', 
+                         'credits', 
+                         'grade', 
+                         'division', 
+                         'instructor'
+                        ]
+        course_dict = dict(zip(course_fields, course_info))
+        course_list.append(course_dict)
+
+    return course_list
+
+
+def calculate_gpa(course_list):
+    """Given a list of course dicts, calculate the GPA."""
+    gpa_equivs = {'A+': 4.0,
+                  'A' : 4.0,
+                  'A-': 3.67,
+                  'B+': 3.33,
+                  'B' : 3.0,
+                  'B-': 2.67,
+                  'C+': 2.33,
+                  'C' : 2.0,
+                  'C-': 1.67,
+                  'D+': 1.33,
+                  'D' : 1.0,
+                  'D-': 0.67,
+                  'F' : 0.0}
+    total_credits = 0
+    equiv_sum = 0
+
+    for course in course_list:
+        credits = float(course['credits'])
+        try:
+            equiv_sum += credits * gpa_equivs[course['grade']]
+        except KeyError: # Skip if it's an invalid grade (e.g., CR/NR, W, 1)
+            continue
+        total_credits += credits
+
+    return equiv_sum / total_credits
