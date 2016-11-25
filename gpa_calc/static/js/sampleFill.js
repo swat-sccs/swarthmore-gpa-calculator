8a2fe8624869fca86900577e4ff720076ce9b9b2
Oliver Newman <oliver@whmsi.com>
2016-11-25 13:38:37 -0500

    Add more classes to sample transcript

--- gpa_calc/static/js/sampleFill.js
+++ gpa_calc/static/js/sampleFill.js
@@ -1,12 +1,39 @@
 function sampleFill() {
-  const sampleGrades = "Term: Spring 2016	Credits Earned:  4\n" +
+  const sampleGrades = 
+    "\n" + 
+    "   Student Records\n" +
+    "\n" + 
+    "Personal Information\n" +
+    "\n" + 
+    "Name:         Student S. Swattie   Class Year:     2019\n" +
+    "Majors:       Economics, Political Science, Chemistry, Biology\n" +
+    "Minors:\n" +
+    "  Honors:     Honors Candidate (3/1)\n" +
+    "College ID:   123456789\n" +
+    "\n" + 
+    "Unofficial Grade Report - including PE courses\n" +
+    "\n" +
+    "Term: Fall 2016	Credits Earned:  4\n" +
     "Course	Course Title	Crse	Grade	Distribution	Instructor/Institution\n" +
-    "CPSC 031 01	Intro to Computer Systems	1	B	NS, NSEP	Webb, Kevin\n" +
-    "CPSC 046 01	Theory of Computation	1	C	NS	Kutty, Sindhu\n" +
-    "ENVS 071 01	Remote Sensing of Environment	1	A+	-	Narayanaraj, G.\n" +
-    "PHED 087V 01	Indoor Track & Field (Men)	0	2	PE	Carroll, Peter\n" +
-    "PHED 098V 01	Outdoor Track & Field (Men)	0	0	PE	Carroll, Peter\n" +
-    "PHIL 001B 01	Intro:Criticism and Cultur (W)	1	D	HU, W	Eldridge, Richard\n";
+    "BIOL 001 01	Cellular & Molecular Biol (W) 	1	B	NS, NSEP, W	Siwicki, Kathleen\n" +
+    "CHEM 010 01	Fdns of Chemical Principles	1	C+	NS, NSEP	Porello, Silvia\n" +
+    "ENGL 009H 01	FYS: Portraits of the Artist (W)	1	CR	HU, W	Schmidt, Peter\n" +
+    "STAT 011 02	Statistical Methods I	1	B+	NS	Chen, Lu\n" +
+    "\n" +
+    "Term: Spring 2016	Credits Earned:  4\n" +
+    "Course	Course Title	Crse	Grade	Distribution	Instructor/Institution\n" +
+    "ECON 021 01	Intermediate Macroeconomics	1	A-	SS	Kuperberg, Mark\n" +
+    "PHIL 001B 01	Intro: Criticism and Culture (W)	1	B+	HU, W	Eldridge, Richard\n" +
+    "POLS 003 01	Comparative Politics	1	A	SS	Malekzadeh, Shervin\n" +
+    "HIST 008C 01	East Africa	1	A	SS	Burke,Timothy\n" +
+    "\n" +
+    "Term: Fall 2015	Credits Earned:  4\n" +
+    "Course	Course Title	Crse	Grade	Distribution	Instructor/Institution\n" +
+    "ECON 001 07	Introduction to Economics	1	A	SS	Bhanot, Syon\n" +
+    "PHIL 010 01	FYS: Questions of Inquiry (W)	1	B	HU, W	Raff, Charles\n" +
+    "POLS 002 01	American Politics	1	A-	SS	Reeves, Keith\n" +
+    "PSYC 001 01	Introduction to Psychology	1	A-	SS	Schneider, Allen\n" +
+    "PHED 002A 02	Fitness Training - Fall 1	0	1		Borbee, Karen\n";
 
   $("#grade-input").val(sampleGrades);
 }
